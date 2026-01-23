5f16b0317ce1c09e885529b21ffb7b3cb516a2be
Cameron Tennant <cameronmst12@gmail.com>
2026-01-23 05:38:30 +0000

    Selection Sort Implementation
    Added selection sort function to preform sorting on an unordered list.

---
+++ SelectionSort.py
@@ -0,0 +1,15 @@
+my_list = [64, 21, 22, 19, 5, 5]
+
+def selection_sort(my_list):
+    for iter in range(0, len(my_list)-1):
+        min = iter
+        for i in range(iter + 1, len(my_list)):
+            if my_list[i] < my_list[min]:
+                min = i
+        my_list[iter], my_list[min] = my_list[min], my_list[iter]
+    return my_list
+
+print(selection_sort(my_list)) # (W3Schools, n.d)
+
+#Reference
+#W3schools, (n.d.). DSA Selection Sort. [online] Available at: https://www.w3schools.com/dsa/dsa_algo_selectionsort.php.[Accessed 23/01/2026].
