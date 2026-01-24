34d16b22230952ee8dc34ec12e7521026df0747b
Cameron Tennant <cameronmst12@gmail.com>
2026-01-24 07:53:13 +0000

    Search 1.0
    Searching Algorithm to find Smallest, Largest, Mode, Median, IQR 1/4 & 3/4
    
    Of a list.

---
+++ Search.py
@@ -0,0 +1,40 @@
+
+
+my_list = [1, 2, 2, 3, 4, 4, 5, 6, 6] #Has to be sorted, because median and IQR are statistics of order.
+
+def Statistical_Search(my_list):
+    
+    #Smallest & Largest
+    smallest = my_list[0]
+    largest = my_list[-1]
+
+    #Median
+    if len(my_list) % 2 == 1:
+        median = my_list[len(my_list)//2]
+    else:
+        median = (my_list[len(my_list)//2 - 1] + my_list[len(my_list)//2]) / 2.
+    
+    #Inter Quartile
+    n = len(my_list)
+    LowerIQF = my_list[n // 4]
+    UpperIQF = my_list[(3 * n) // 4]
+
+    #Mode
+    max_count = 0
+    modes = []
+    for item in my_list:
+        count = my_list.count(item)
+        if count > max_count:
+            max_count = count
+            modes = [item]
+        elif count == max_count and item not in modes:
+            modes.append(item) #(GeeksforGeeks, 2018)
+
+    return smallest, largest, median, LowerIQF, UpperIQF, modes
+
+results = Statistical_Search(my_list)
+print(f'\nSmallest: {results[0]}\nLargest: {results[1]}\nMedian: {results[2]}\nLower IQF: {results[3]}\nUpper IQF: {results[4]}\nMode: {results[5]} \n')
+
+#References
+
+#GeeksforGeeks (2018). Finding Mean, Median, Mode in Python without libraries. [online] GeeksforGeeks. Available at: https://www.geeksforgeeks.org/python/finding-mean-median-mode-in-python-without-libraries/.
