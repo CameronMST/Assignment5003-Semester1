4094be17e1c0a272efd2a226595e465f4773943c
Cameron Tennant <cameronmst12@gmail.com>
2026-01-24 11:42:28 +0000

    MergeSort 2.0
    Added ascending and descending capabilities using input to Merge Sort.

--- MergeSort.py
+++ MergeSort.py
@@ -1,3 +1,5 @@
+equality = input("Enter \'+\' For Ascending or \'-\' For Descending: ")
+
 def Merge_Sort(my_list):
     if len(my_list) > 1:
         mid = len(my_list) // 2
@@ -12,13 +14,29 @@ def Merge_Sort(my_list):
         left_index, right_index, main_index = 0, 0, 0
 
         while left_index < len(left_half) and right_index < len(right_half):
-            if left_half[left_index] < right_half[right_index]:
-                my_list[main_index] = left_half[left_index]
-                left_index += 1
+            
+            #Descending Order
+            if equality == '-':
+                if left_half[left_index] > right_half[right_index]:
+                    my_list[main_index] = left_half[left_index]
+                    left_index += 1
+                    main_index += 1
+            
+                else:
+                    my_list[main_index] = right_half[right_index]
+                    right_index += 1
+                    main_index += 1
+            #Ascending Order
             else:
-                my_list[main_index] = right_half[right_index]
-                right_index += 1
-            main_index += 1
+                if left_half[left_index] < right_half[right_index]:
+                    my_list[main_index] = left_half[left_index]
+                    left_index += 1
+                    main_index += 1
+
+                else:
+                    my_list[main_index] = right_half[right_index]
+                    right_index += 1
+                    main_index += 1    
 
         while left_index < len(left_half):
             my_list[main_index] = left_half[left_index]
@@ -28,7 +46,7 @@ def Merge_Sort(my_list):
         while right_index < len(right_half):
             my_list[main_index] = right_half[right_index]
             right_index += 1
-            main_index += 1
+            main_index += 1 #(Tutorialspoint, 2019)
 
     return my_list
 
@@ -37,4 +55,4 @@ print(Merge_Sort(my_list))
 
 #References
 
-#Tutorialspoint.com. (2019). Data Structures - Merge Sort Algorithm - Tutorialspoint. [online] Available at: https://www.tutorialspoint.com/data_structures_algorithms/merge_sort_algorithm.htm.
+#Tutorialspoint, (2019). Data Structures - Merge Sort Algorithm - Tutorialspoint. [online] Available at: https://www.tutorialspoint.com/data_structures_algorithms/merge_sort_algorithm.htm.
