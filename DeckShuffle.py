0089285e8267ad32810c8d14d2ca4eadd0600999
Cameron Tennant <cameronmst12@gmail.com>
2026-01-24 11:56:04 +0000

    DeckShuffler 1.0
    Working randomiser, prototype using import random.
    
    I may chance this, as I do not believe the assignment will allow import random.

---
+++ DeckShuffle.py
@@ -0,0 +1,21 @@
+import random
+
+Type = ['Heart', 'Diamond', 'Club', 'Spade']
+Ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+
+deck = [(Type, Rank) for Type in Type 
+        for Rank in Ranks] #(Portfolio Courses, 2023)
+
+seed = random.randint(1, 1000)
+random.seed(seed)
+
+for i in range(len(deck)):
+    j = random.randint(0, len(deck) - 1)
+    deck[i], deck[j] = deck[j], deck[i]
+
+print(*deck, sep="\n")
+
+
+#References
+
+#Portfolio Courses (2023). Shuffle A Deck Of Cards | Python Example. [online] YouTube. Available at: https://www.youtube.com/watch?v=9Lh4PAN1HsA [Accessed 24 Jan. 2026].
