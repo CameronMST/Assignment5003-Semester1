cb70338723e491258c453d7ceae78849f520b055
Cameron Tennant <cameronmst12@gmail.com>
2026-01-26 05:07:10 +0000

    TestFibonnaci
    Produce File for Test Fibonacci

---
+++ TestFibbonaci.py
@@ -0,0 +1,20 @@
+def fib_memo(n, mem={}):
+    if n in mem:
+        return mem[n]
+    if n <=1:
+        mem[n] = n
+    else:
+        mem[n] = fib_memo(n-1, mem) + fib_memo(n-2, mem)
+    return mem[n]
+
+print(fib_memo(2))
+#(CodeLucky, 2025)
+
+
+
+
+
+#-------------------------------
+#References
+
+#CodeLucky (2025). Fibonacci Sequence: Classic Dynamic Programming Example with Python - CodeLucky. [online] CodeLucky. Available at: https://codelucky.com/fibonacci-sequence/ [Accessed 23 Jan. 2026].
