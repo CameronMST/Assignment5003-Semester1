0fd70d58ecac9aa0d0302df0cb960f3cb81ad96d
Cameron Tennant <cameronmst12@gmail.com>
2026-01-26 08:14:07 +0000

    RSA Complexity Test
    Added Complexity Test for RSA

---
+++ ComplexityRSA.py
@@ -0,0 +1,88 @@
+import time
+import tracemalloc
+
+def RSAAlgorithm():
+    time_start = time.time()
+    tracemalloc.start()
+
+    def power(base, exponent, m):
+        result = 1
+        base = base % m
+        while exponent > 0:
+            if exponent & 1:
+                result = (result * base) % m
+            base = (base * base) % m
+            exponent = exponent // 2
+        return result
+
+    def isPrime(n):
+        if n <= 1: 
+            return False
+        for i in range(2, n): 
+            if n % i == 0: 
+                return False; 
+        return n; 
+
+    def modInverse(e, phi):
+        for d in range(2, phi):
+            if (d * e) % phi == 1:
+                return d
+        return None
+
+    def generateKeys(p, q):
+        n = p * q
+        phi = (p - 1) * (q - 1)
+        e = 0
+        for e in range(2, phi):
+            if gcd(e, phi) == 1:
+                break
+        d = modInverse(e, phi)
+        return (e, n), (d, n)
+
+    def gcd(inta, intb):
+        while intb !=0:
+            inta, intb = intb, inta % intb
+        return inta
+        
+    def encrypt(pk, plaintext):
+        key, n = pk
+        cipher = [power(ord(char), key, n) for char in plaintext]
+        return " ".join(map(str, cipher))
+
+    def decrypt(pk, ciphertext):
+        key, n = pk
+        try:
+            ciphertext = list(map(int, ciphertext.split()))
+            plain = [chr(power(char, key, n)) for char in ciphertext]
+            return ''.join(plain)
+        except ValueError:
+            print("Ciphertext is not valid!")
+            return
+    
+
+    Input1 = 17 #P
+    Input2 = 23 #Q
+
+    input3 = 2 #1 For Encrypt 2 For Decrypt
+    Message = "234 16 301 301 304" #Enter Message or CipherText
+
+    public, private = generateKeys(Input1, Input2)
+
+    if input3 == 1:
+        Encrypted_Message = encrypt(public, Message)
+        print(f"Original Message: {Message}")
+        print(f"Encrypted Message: {Encrypted_Message}")
+
+    elif input3 == 2:
+        Decrypted_Message = decrypt(private, Message)
+        print(f"Encrypted Message: {Message}")
+        print(f"Decrypted Message: {Decrypted_Message}")
+
+    else:
+        print("Invalid option selected.")
+
+    current, peak = tracemalloc.get_traced_memory()
+
+    print(f"\n--- Execution Time: {(time.time() - time_start) * 1000:.3f} miliseconds ---")
+    print(f"--- Memory Usage: {peak:.2f} bytes ---")
+RSAAlgorithm()
